2b84bc0288e680d0361d158579d33575a691fa77
Omid Ehsan <omid6460@live.dk>
2022-03-16 13:51:25 +0100

    Login and signup

---
+++ TrackTraining/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace TrackTraining
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext()
+            : base("MyConnection", throwIfV1Schema: false)
+        {
+        }
+    }
+}
---
+++ TrackTraining/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackTraining
+{
+    public class Startup
+    {
+        public static Func<UserManager<AppUser>> UserManagerFactory { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            // this is the same as before
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/auth/login")
+            });
+
+            // configure the user manager
+            UserManagerFactory = () =>
+            {
+                var usermanager = new UserManager<AppUser>(
+                    new UserStore<AppUser>(new AppDbContext()));
+                // allow alphanumeric characters in username
+                usermanager.UserValidator = new UserValidator<AppUser>(usermanager)
+                {
+                    AllowOnlyAlphanumericUserNames = false
+                };
+
+                return usermanager;
+            };
+        }
+    }
+}
