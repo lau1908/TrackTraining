3930f1b6baf577fcf65972f4c69f2118f6d76feb
Omid Ehsan <omid6460@live.dk>
2022-04-13 16:06:40 +0200

    hej med digf, det der graf noiget

---
+++ TrackTraining/DBModels/OvelseRekorder2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackTraining.DBModels
+{
+    public class OvelseRekorder2
+    {
+
+        public string OvelseNavn { get; set; }
+        public string Billede { get; set; }
+        public int OvelseId { get; set; }
+        public DateTime dato { get; set; }
+        public int gentagelser { get; set; }
+       
+
+
+
+    }
+}
