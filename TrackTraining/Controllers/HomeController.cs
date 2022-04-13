a5f2ff87b90f6918ac9436e325a818a5afeb4f10
Lau Løgager Sørensen <laux0488@college360.dk>
2022-04-13 16:10:15 +0200

    leader

--- TrackTraining/Controllers/HomeController.cs
+++ TrackTraining/Controllers/HomeController.cs
@@ -21,9 +21,8 @@ namespace TrackTraining.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IEnumerable<Rekorder2> Leaders = Database.Rekorder2.OrderByDescending(e => e.Gentagelser);
+            return View(Leaders);
         }
         public ActionResult Contact()
         {
