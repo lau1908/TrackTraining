18a68e0ba1948a6a105d76688b83afb706bf6d94
Lau Løgager Sørensen <laux0488@college360.dk>
2022-04-16 15:03:57 +0200

    Kommentering og slettelse af ligegyldige ting

--- TrackTraining/Controllers/HomeController.cs
+++ TrackTraining/Controllers/HomeController.cs
@@ -24,13 +24,6 @@ namespace TrackTraining.Controllers
             IEnumerable<Rekorder2> Leaders = Database.Rekorder2.OrderByDescending(e => e.Gentagelser);
             return View(Leaders);
         }
-        public ActionResult Contact()
-        {
-            string uid = User.Identity.GetUserId();//erklærer "uid" til at indholde brugerens ID
-            IEnumerable<Rekorder2> rekorder = Database.Rekorder2.OrderBy(e => e.dato).Where(e => e.OvelseId == 4 && e.BrugerId == uid).AsEnumerable();
-            return View(rekorder);//retunerer rekorder som outputargument 
-        }
-
        
         [HttpPost]
         public ActionResult Contact(DateTime date, int Gentagelser, int Id)//bruger date og gentagelser som inputargument
@@ -47,16 +40,15 @@ namespace TrackTraining.Controllers
                 Gentagelser = Gentagelser,  
                 dato = date,
             };
-            Database.Rekorder2.Add(Rekord);//tilføjer den nye data 
+            Database.Rekorder2.Add(Rekord);//tilføjer den nye data til databasen
             Database.SaveChanges();//gemmer ændringer 
-            return Json(Rekord); 
+            return Json(Rekord); //Retunerer objektet Rekord som en Json
             
         }
 
 
 
-            public ActionResult Ovelser()
-        {
+            public ActionResult Ovelser(){
             ViewBag.primære = Database.Ovelsers.Where(e=>e.Primære.ToLower()!= "null").ToList();
 
             return View();
@@ -66,26 +58,11 @@ namespace TrackTraining.Controllers
         {
             string uid = User.Identity.GetUserId(); //erklærer "uid" til at indholde brugerens ID
             Ovelser øvelse = Database.Ovelsers.FirstOrDefault(e => e.OvelseId == ovelseId);
-            ExerciseData data = new ExerciseData(
+            ExerciseData data = new ExerciseData(//laver et objekt af klassen ExerciseData 
                 Database.Ovelsers.FirstOrDefault(e => e.OvelseId == ovelseId).Rekorder2.Where(e => e.BrugerId == uid),
                 øvelse.OvelseNavn,
                 øvelse.OvelseId
             );
-              
-            //{
-            //    Ovelsers = Database.Ovelsers.Where(e => e.OvelseId == ovelseId).ToList(),
-            //    Rekorder2 = Database.Rekorder2.OrderBy(e => e.dato).Where(e => e.OvelseId == 4 && e.BrugerId == uid).ToList()
-            //};
-            //List<Rekorder2> rekorder = Database.Rekorder2.OrderBy(e => e.dato).Where(e => e.OvelseId == ovelseId && e.BrugerId == uid).ToList();
-            //List<Ovelser> ovelsers= Database.Ovelsers.Where(e => e.OvelseId == ovelseId).ToList();
-            //var øvelser = from r in rekorder
-            //              join o in ovelsers on r.OvelseId equals o.OvelseId into table1
-            //              from o in table1.ToList()
-            //              select new OvelseRekorder2
-            //              {
-            //                    ovelsers= o,
-            //                    rekorder2= r,
-            //              };
             return View(data);
         }
     }
--- TrackTraining/Models/ExerciseData.cs
+++ TrackTraining/Models/ExerciseData.cs
@@ -6,11 +6,12 @@ using TrackTraining.DBModels;
 
 namespace TrackTraining.Models
 {
-    public class ExerciseData
+    public class ExerciseData//ExerciseData er en klasse som er lavet for at struktrurerer dataen så vi kan tilgå den under "Øvelser.cshtml"
     {
-        public IEnumerable<Rekorder2> data { get; set; }
+        public IEnumerable<Rekorder2> data { get; set; } 
         public string name { get; set; }
         public int id { get; set; }
+
         public ExerciseData(IEnumerable<Rekorder2> data, string name, int id) 
         {
             this.data = data;
